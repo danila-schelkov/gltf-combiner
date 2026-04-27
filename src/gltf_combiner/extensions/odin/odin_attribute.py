16467cffe9c7e3321ef5ce44b6357f261e765733
Danila Schelkov <me@donutquine.dev>
2026-04-28 01:45:06 +0300

    refactor: more types in odin_attribute.py

--- src/gltf_combiner/extensions/odin/odin_attribute.py
+++ src/gltf_combiner/extensions/odin/odin_attribute.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numpy.typing as npt
 
 from .attribute_format import OdinAttributeFormat
 from .attribute_type import OdinAttributeType
@@ -11,13 +12,13 @@ class OdinAttribute:
         attribute_format: OdinAttributeFormat,
         offset: int,
     ) -> None:
-        self.type = attribute_type
-        self.format = attribute_format
-        self.offset = offset
+        self.type: OdinAttributeType = attribute_type
+        self.format: OdinAttributeFormat = attribute_format
+        self.offset: int = offset
 
-        self._dtype = self.format.to_numpy_dtype()
-        self._elements_count = self.format.to_element_count()
-        self._normalized = self.type.is_normalized()
+        self._dtype: np.dtype = self.format.to_numpy_dtype()
+        self._elements_count: int = self.format.to_element_count()
+        self._normalized: bool = self.type.is_normalized()
 
     @property
     def data_type(self) -> np.dtype:
@@ -27,7 +28,7 @@ class OdinAttribute:
     def elements_count(self) -> int:
         return self._elements_count
 
-    def read(self, data: bytes, offset: int) -> np.ndarray:
+    def read(self, data: bytes, offset: int) -> npt.NDArray[np.number]:
         match self.format:
             case OdinAttributeFormat.NormalizedWeightVector:
                 value = np.frombuffer(data, dtype=np.uint32, offset=offset, count=1)[0]
